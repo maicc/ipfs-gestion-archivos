89cbd23dd4adc473a8f235928810f083d74ff25b
juan <juanci123z@gmail.com>
2026-03-20 15:11:50 -0500

    fix: Arreglando variables v2

--- oguripfs-backend/src/controller/uploadR2.controller.ts
+++ oguripfs-backend/src/controller/uploadR2.controller.ts
@@ -13,7 +13,7 @@ import axios from "axios"
 // Configuración de tu cliente R2
 const s3Client = new S3Client({
     region: "auto",
-    endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
         accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
