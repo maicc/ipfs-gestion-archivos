ae2661c1fcda2e439d0a9c7c1889510a3d7e7081
juan <juanci123z@gmail.com>
2026-03-24 13:03:32 -0500

    test: Adding 1000000000 de propina

--- oguripfs-backend/src/services/crustPinning.service.ts
+++ oguripfs-backend/src/services/crustPinning.service.ts
@@ -87,7 +87,7 @@ export const confirmarSubida = async (fileInfo: FileDataPayload) => {
 export async function placeStorageOrder(
     cid: string,
     fileSize: number,
-    tips: number = 0,
+    tips: number = 1000000000,
     memo: string = ''
 ): Promise<CrustOrderResult> {
     const crustApi = await initCrustApi();
